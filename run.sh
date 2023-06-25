7d76e3ce7db16b185c5eea1e3d9be71ce1d92c8a
Frederick Wong <77422662+frederickcwong@users.noreply.github.com>
2023-06-24 20:04:52 -0700

    feat: add job notification & available space check. Closes: #2, #5

--- run.sh
+++ run.sh
@@ -38,12 +38,61 @@ echo
 echo "+------------------------------+"
 echo "| tar'ing...                   |"
 echo "+------------------------------+"
-tar czf ${MEDIA_DIR}/${TAR_TARGET} -C ${WORKING_DIR} data
 
-if test -f ${MEDIA_DIR}/${TAR_TARGET}; then
-        echo "Backup file created: $TAR_TARGET"
+MESSAGE_TITLE="No message title"
+MESSAGE_BODY="No message body"
+
+# create the tar ball
+tar czf ${SCRIPT_DIR}/${TAR_TARGET} -C ${WORKING_DIR} data
+
+# obtain backup storage space availability and space needed
+SPACE_AVAILABLE=$(df $MEDIA_DIR | tail -1 | awk '{print $4}')
+SPACE_NEEDED=$(du ${SCRIPT_DIR}/${TAR_TARGET} | awk '{print $1}')
+
+# move the tarball to backup storage
+if [[ $SPACE_AVAILABLE -gt $SPACE_NEEDED ]]; then
+        mv ${SCRIPT_DIR}/${TAR_TARGET} ${MEDIA_DIR}
+        if test -f ${MEDIA_DIR}/${TAR_TARGET}; then
+                MESSAGE_TITLE="Success: backup file created: $TAR_TARGET"
+                MESSAGE_BODY="Backup file moved to storage media successfully."
+        else
+                MESSAGE_TITLE="Failed: error creating backup: $TAR_TARGET"
+                MESSAGE_BODY="Backup file created but failed moving to the storage media."
+        fi
 else
-        echo "Error creating backup file: $TAR_TARGET"
+        MESSAGE_TITLE="Failed: insufficient storage space"
+        MESSAGE_BODY="`du -h ${SCRIPT_DIR}/${TAR_TARGET} | \
+                awk '{print \"Storage needed: \"$1}'` `df -h $MEDIA_DIR | \
+                tail -1 | awk '{print \" (Total: \"$2\", Used: \"$3\", Avail: \"$4\")\"}'`"
+fi
+
+# determine if disk space is below the threshold
+AVAIL_MB=$(df -BM ${MEDIA_DIR} | tail -1 | awk '{print $4}' | sed 's/M//')
+if [[ $AVAIL_MB -lt $WARNING_THRESHOLD_MB ]]; then
+        MESSAGE_BODY=$(cat <<EOF
+${MESSAGE_BODY}
+!!! Warning: Available space is less than ${WARNING_THRESHOLD_MB}M !!!
+EOF
+)
+fi
+
+# output result to console log
+echo $MESSAGE_TITLE
+echo $MESSAGE_BODY
+
+# use shoutrrr to send notification
+if [[ ! -z "$SHOUTRRR_URL" ]]; then
+        echo
+        echo "+------------------------------+"
+        echo "| Sending notification...      |"
+        echo "+------------------------------+"
+        echo "Sending notification using shoutrrr..."
+        MESSAGE_BODY=$(cat <<EOF
+${MESSAGE_BODY}
+Backup completed on: `date +"%F-%H-%M"`
+EOF
+)
+        ${SCRIPT_DIR}/shoutrrr send -u "${SHOUTRRR_URL}" -t "${MESSAGE_TITLE}" -m "${MESSAGE_BODY}"
 fi
 
 echo
